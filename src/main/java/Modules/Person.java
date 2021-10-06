1a53c1500a07e5d9c2ca11a09585b842f7ea86fa
Geo1981 <georgii81@gmail.com>
2021-10-07 00:11:28 +0300

    HomeWork5

--- src/main/java/Modules/Person.java
+++ src/main/java/Modules/Person.java
@@ -1,4 +1,23 @@
 package Modules;
 
 public class Person {
+    private String fullName;
+    private String position;
+    private String email;
+    public int age;
+
+    public Person(String fullName, String position, String email, int age) {
+        this.fullName = fullName;
+        this.position = position;
+        this.email = email;
+        this.age = age;
+    }
+
+    public void showPerson() {
+        System.out.println("Сотрудик. ФИО: " + fullName + ", должность: " + position + ", email: " + email + ", возраст: " + age + ".");
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
